06f0d0694cfcf1f716a6b9203a00aaafb57c0dee
Noe Calmes <noecalmes@mac.home>
2026-02-06 20:55:54 +0100

    add 4 eme page

--- src/components/Screenshots.tsx
+++ src/components/Screenshots.tsx
@@ -1,6 +1,7 @@
 import image1 from '../assets/image/1.png';
 import image2 from '../assets/image/2.png';
 import image3 from '../assets/image/3.png';
+import image4 from '../assets/image/4.png';
 
 interface ScreenshotsProps {
   lang: 'en' | 'fr';
@@ -17,7 +18,7 @@ export default function Screenshots({ lang }: ScreenshotsProps) {
   };
 
   const text = content[lang];
-  const screenshots = [image1, image2, image3];
+  const screenshots = [image1, image2, image3, image4];
 
   return (
     <section className="py-20 px-6 bg-plouf-background">
